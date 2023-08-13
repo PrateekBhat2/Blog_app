4ca55410d56493d6e263d890d0d3b17d4429fddc
Prateek Bhat <prateekbhat255@gmail.com>
2023-08-13 21:36:53 +0530

    deploy config

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/images",express.static(path.join(__dirname,"/images")));
 
+const port = process.env.PORT || 5000
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -42,6 +44,17 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-    console.log("Backend is running");
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+
+app.listen(port, () => {
+    console.log("Backend is running", port);
 });
